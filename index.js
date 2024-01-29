fbb40b925ec8288021b39827c8ca6b5aec11bf08
Shtantzer <seke.lovro@gmail.com>
2024-01-29 22:41:15 +0100

    Keeping track of a game start (keypress event) using a boolean so the nextSequence() executes only the first time and the level number in <h1> increments accordingly

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 //Step2: znao za koncept "variable scope" ali ovdje svejedno nisam znao sto postavit u globalni pristup a sto u funkcijski pristup, konkretno nisam shvatio gdje bi smjestio randomChosenColor i gamePattern
+//Step7: (nisam pogledo hintove) nisam znao kako odrediti da li se funkcija izvrsila. Stavi se "false" u varijablu "started" zatim se u anonimnoj key event funkciji postavi if upit
 
 buttonColors = ["red", "blue", "green", "yellow"];
 gamePattern = [];
 userClickedPattern = [];
+level = 0;
+started = false; 
 
+$(document).keypress(function() {
+    if (!started) {
+        $("#level-title").text("Level " + level)
+        nextSequence();
+        started = true;
+    }
+});
 
 function nextSequence() {
+    level ++;
+    $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random(randomNumber) * 4); //nije potrebno staviti randomNumber u .random metodu jer radi bez konteksta
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
