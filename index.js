bf57e56d87b95b6bba0438476ff55912c10a6746
unknown <seke.lovro@gmail.com>
2024-01-30 13:08:21 +0100

    Making a checkAnswer() function will compare user-clicked sequence with the generated sequence; calls the next sequence with a delay and then empties the user-clicked sequence

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Step2: znao za koncept "variable scope" ali ovdje svejedno nisam znao sto postavit u globalni pristup a sto u funkcijski pristup, konkretno nisam shvatio gdje bi smjestio randomChosenColor i gamePattern
 //Step7: (nisam pogledo hintove) nisam znao kako odrediti da li se funkcija izvrsila. Stavi se "false" u varijablu "started" zatim se u anonimnoj key event funkciji postavi if upit
-
+//Step8: nisam shvacao logiku currentLevela, ali onda je sinulo: "ZA USPOREDBU CURRENTLEVELA SE KORISTI ISTI INDEX KAO ARGUMENT(checkAnswer(userClickedPattern.length-1);) KOJI SE DODJELJUJE gamePattern i userClickedPattern RADI ITERACIJE ISTOG INDEXA TO JEST ZADNJE BOJE U LISTI"
 buttonColors = ["red", "blue", "green", "yellow"];
 gamePattern = [];
 userClickedPattern = [];
@@ -17,10 +17,12 @@ $(document).keypress(function() {
 
 function nextSequence() {
     level ++;
+    userClickedPattern = [];
     $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random(randomNumber) * 4); //nije potrebno staviti randomNumber u .random metodu jer radi bez konteksta
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
+    console.log(gamePattern);
     $("#" + randomChosenColor).animate({
         opacity: 0
     }, 100)
@@ -37,6 +39,9 @@ $(".btn").click(function(e) {
     console.log(userClickedPattern);
     playSound(userChosenColor);
     animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length-1); /* ovdje sam iz ne shvacanja logike 
+    stavio "userClickedPattern.slice(0, -1)" sto nema veze s onim sto meni treba 
+    a zbog toga mi dosta dugo igra nije radila *facepalm* */
 })
 
 function playSound(name) {
@@ -49,4 +54,17 @@ function animatePress(currentColor) {
     setTimeout(function() {
         $("#" + currentColor).removeClass("pressed");
     },100);
+}
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("success");
+        if (gamePattern.length === userClickedPattern.length) {
+            setTimeout(nextSequence(),
+            1000);
+        }
+    }
+    else {
+        console.log("wrong");
+    }
 }
